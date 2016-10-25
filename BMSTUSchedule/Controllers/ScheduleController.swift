b863070759c642fb53bd5f7d715aed1ff7ba2945
Artem Belkov <bestK1ngArthur@aol.com>
2016-10-26 00:00:55 +0300

    Добавил slide-menu

---
+++ BMSTUSchedule/Controllers/ScheduleController.swift
@@ -0,0 +1,41 @@
+//
+//  ScheduleController.swift
+//  BMSTUSchedule
+//
+//  Created by Artem Belkov on 25/10/2016.
+//  Copyright © 2016 techpark-iOS. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleController: UIViewController {
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
