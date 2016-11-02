86d126dc139420b4dacece75efaf815d0f5a9786
Artem Belkov <bestK1ngArthur@aol.com>
2016-11-02 19:39:54 +0300

    Change selection color in menu

--- BMSTUSchedule/Controllers/MenuController.swift
+++ BMSTUSchedule/Controllers/MenuController.swift
@@ -10,20 +10,19 @@ import UIKit
 
 class MenuController: UITableViewController {
 
+    
+    let menuItemSelectedColor = UIColor(red: 87/255, green: 108/255, blue: 126/255, alpha: 1)
+
     @IBOutlet var imageViews: [UIImageView]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Change default image's tint color
         for imageView: UIImageView in self.imageViews {
             let color = UIColor(red: 161/255, green: 172/255, blue: 182/255, alpha: 1)
             imageView.image = imageView.image?.imageWithColor(newColor: color)
         }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
@@ -34,6 +33,26 @@ class MenuController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        
+        let selectedCell:UITableViewCell = tableView.cellForRow(at: indexPath as IndexPath)!
+        
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = menuItemSelectedColor
+        selectedCell.selectedBackgroundView = backgroundView
+        
+        return indexPath
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let selectedCell:UITableViewCell = tableView.cellForRow(at: indexPath as IndexPath)!
+        
+        selectedCell.contentView.backgroundColor = menuItemSelectedColor
     }
 
 }
