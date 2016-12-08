e2d99f40ce0581350992f19ff3e63a6035514494
Artem Belkov <bestK1ngArthur@aol.com>
2016-12-08 23:33:18 +0300

    Add group cell

--- BMSTUSchedule/Controllers/GroupsController.swift
+++ BMSTUSchedule/Controllers/GroupsController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class GroupsController: UIViewController {
+class GroupsController: UITableViewController {
 
     @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    var groups: [Group] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +23,38 @@ class GroupsController: UIViewController {
             menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        // Remove empty cells
+        tableView.tableFooterView = UIView()
+        
+        // Load groups
+        Manager.firebaseManager.getGroups { (groups: [Group]) in
+            self.groups = groups
+            self.tableView.reloadData()
+        }
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groups.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GroupCell", for: indexPath) as! GroupCell
+        
+        let group = self.groups[indexPath.row]
+        
+        // Set group info
+        cell.nameLabel.text = group.name
+ 
+        return cell
     }
 
     // MARK: - Memory
---
+++ BMSTUSchedule/Views/GroupCell.swift
@@ -0,0 +1,26 @@
+//
+//  GroupCell.swift
+//  BMSTUSchedule
+//
+//  Created by Artem Belkov on 08/12/2016.
+//  Copyright © 2016 techpark-iOS. All rights reserved.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
