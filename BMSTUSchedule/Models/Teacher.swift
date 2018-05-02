d652e08144fdab7ecee7a263c1f064b3c2a164a1
a.belkov <bestK1ngArthur@aol.com>
2018-05-02 17:20:52 +0300

    Add teacher

---
+++ BMSTUSchedule/Models/Teacher.swift
@@ -0,0 +1,36 @@
+//
+//  Teacher.swift
+//  BMSTUSchedule
+//
+//  Created by a.belkov on 02/05/2018.
+//  Copyright © 2018 BMSTU Team. All rights reserved.
+//
+
+import Foundation
+
+/*
+ Teacher 👨‍🏫
+ */
+class Teacher: CustomStringConvertible {
+
+    var firstName: String
+    var lastName: String
+    var middleName: String?
+    
+    var department: String
+    
+    var photoURL: URL?
+    var about: String?
+
+    var description : String {
+        return "Teacher(\"\(firstName) \(lastName)\")\n"
+    }
+    
+    // MARK: Initialization
+    
+    init(firstName: String, lastName: String, department: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.department = department
+    }
+}
