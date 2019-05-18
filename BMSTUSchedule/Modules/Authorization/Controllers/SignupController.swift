54843d018ff34e3729c88e5add4fed29dd4477c1
Artem Belkov <bestK1ngArthur@aol.com>
2019-05-19 00:20:58 +0300

    Experiments with UI

--- BMSTUSchedule/Modules/Authorization/Controllers/SignupController.swift
+++ BMSTUSchedule/Modules/Authorization/Controllers/SignupController.swift
@@ -16,14 +16,8 @@ class SignupController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 4
     }
 
     /*
